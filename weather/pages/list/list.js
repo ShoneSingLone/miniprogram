82aa12b470ba1139f25396286440b1afdd057da8
ShoneSingLone <ShoneSingLone@gmail.com>
2018-06-16 23:56:20 +0800

    update

--- weather/pages/list/list.js
+++ weather/pages/list/list.js
@@ -1,5 +1,16 @@
-// pages/list.js
+const dayMap = ['星期日', '星期一', '星期二', '星期三', '星期四', '星期五', '星期六']
+
 Page({
+  date: {
+    weekWeather: [],
+    city: '广州市'
+  },
+  onLoad(options) {
+    this.setData({
+      city: options.city
+    })
+    this.getWeekWeather()
+  },
   onPullDownRefresh() {
     this.getWeekWeather(() => {
       wx.stopPullDownRefresh()
@@ -10,7 +21,7 @@ Page({
       url: 'https://test-miniprogram.com/api/weather/future',
       data: {
         time: new Date().getTime(),
-        city: "广州市"
+        city: this.data.city
       },
       success: res => {
         let result = res.data.result
@@ -22,8 +33,6 @@ Page({
     })
   },
   setWeekWeather(result) {
-    const dayMap = ['星期日', '星期一', '星期二', '星期三', '星期四', '星期五', '星期六'];
-
     let weekWeather = []
     for (let i = 0; i < 7; i++) {
       let date = new Date()
@@ -39,70 +48,5 @@ Page({
     this.setData({
       weekWeather: weekWeather
     })
-  },
-
-
-  /**
-   * 页面的初始数据
-   */
-  data: {
-    weekWeather: []
-  },
-
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-    this.getWeekWeather();
-
-  },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
   }
 })
