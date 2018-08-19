a01786ac771ffc9ada7780b7e49ba5a250631f56
ShoneSingLone <ShoneSingLone@gmail.com>
2018-08-19 15:09:25 +0800

    backup 改为调用wafer

--- assistant/app.js
+++ assistant/app.js
@@ -26,48 +26,20 @@ App({
     })
     // 展示本地存储能力
     var logs = wx.getStorageSync('logs') || []
-    logs.unshift(Date.now())
+    logs.unshift('launch' + Date.now())
     wx.setStorageSync('logs', logs)
-
-    // 获取用户信息
-  },
-  backup: function () {
-    // 展示本地存储能力
-    var logs = wx.getStorageSync('logs') || []
-    logs.unshift(Date.now())
-    wx.setStorageSync('logs', logs)
-    console.log('logs', logs)
-  },
-  getSettings() {
-    // 获取用户信息
-    wx.getSetting({
-      success: res => {
-        console.log('wx.getSetting', res)
-        if (res.authSetting['scope.userInfo']) {
-          // 已经授权，可以直接调用 getUserInfo 获取头像昵称，不会弹框
-          wx.getUserInfo({
-            success: res => {
-              // 可以将 res 发送给后台解码出 unionId
-              this.data.userInfo = res.userInfo
-              console.log('wx.getUserInfo', res)
-              // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
-              // 所以此处加入 callback 以防止这种情况
-              if (this.userInfoReadyCallback) {
-                this.userInfoReadyCallback(res)
-              }
-            }
-          })
-        }
-      }
-    })
   },
   login({
     success,
     error
   }) {
+    wx.showLoading({
+      title: '正在登录...',
+    })
     wx.getSetting({
-      success: res => {
-        if (res.authSetting['scope.userInfo'] === false) {
+      success: wxSetting => {
+        console.log('getSetting', wxSetting)
+        if (wxSetting.authSetting['scope.userInfo'] === false) {
           this.data.locationAuthType = UNAUTHORIZED
           // 已拒绝授权
           wx.showModal({
@@ -79,14 +51,11 @@ App({
         } else {
           // 已经授权，可以直接调用 getUserInfo 获取头像昵称，不会弹框
           this.data.locationAuthType = AUTHORIZED
-          wx.showLoading({
-            title: '正在登录...',
-          })
           wx.getUserInfo({
-            success: res => {
-              // 可以将 res 发送给后台解码出 unionId
-              this.data.userInfo = res.userInfo
-              console.log(this.data.userInfo)
+            success: (wxUserInfo) => {
+              // 可以将 wxUserInfo 发送给后台解码出 unionId
+              this.data.userInfo = wxUserInfo.userInfo
+              console.log('wxUserInfo', wxUserInfo)
               // 获取code=>userName，userPwd换取 Token{初次token生成，再次token验证}
               wx.login({
                 success: ({
@@ -101,7 +70,8 @@ App({
                     data: {
                       endpoint: 'login',
                       action: 'getWeChatToken',
-                      code
+                      code,
+                      wxUserInfo
                     },
                     success: res => {
                       console.log('remote login success', res)
